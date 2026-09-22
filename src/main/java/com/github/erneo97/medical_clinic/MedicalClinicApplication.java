ba4e76ccf0b01d626cff7d2dcbefd75613af6f32
Erneo97 <michalkaniewski1997@gmail.com>
2026-09-22 22:15:43 +0200

    dock: checking witch classes are in the Spring container

--- src/main/java/com/github/erneo97/medical_clinic/MedicalClinicApplication.java
+++ src/main/java/com/github/erneo97/medical_clinic/MedicalClinicApplication.java
@@ -3,11 +3,17 @@ package com.github.erneo97.medical_clinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MedicalClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicalClinicApplication.class, args);
+//		SpringApplication.run(MedicalClinicApplication.class, args);
+		var context = SpringApplication.run(MedicalClinicApplication.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).sorted()
+				.forEach(System.out::println);
+
 	}
 
 }
