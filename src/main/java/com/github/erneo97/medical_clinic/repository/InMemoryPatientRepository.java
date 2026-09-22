30dc44aafc1c3c3b75bc3d2788e2ae4e7f5a5bcf
Erneo97 <michalkaniewski1997@gmail.com>
2026-09-22 22:09:01 +0200

    feat: create First repository in memory (List)

---
+++ src/main/java/com/github/erneo97/medical_clinic/repository/InMemoryPatientRepository.java
@@ -0,0 +1,38 @@
+package com.github.erneo97.medical_clinic.repository;
+
+import com.github.erneo97.medical_clinic.model.Patient;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class InMemoryPatientRepository {
+    private final AtomicLong counter = new AtomicLong(1);
+    private final List<Patient> patients = new ArrayList<>();
+
+    public List<Patient> findAll() {return List.copyOf(patients);}
+
+    public Optional<Patient> findById(int id) {
+        return patients.stream()
+                .filter(patient -> patient.getId() == id)
+                .findFirst();
+    }
+
+    public Optional<Patient> findByEmail(String email) {
+        return patients.stream()
+                .filter(patient -> patient.getEmail().equals(email))
+                .findFirst();
+    }
+
+    public void save(Patient patient) {
+        patient.setId(counter.getAndIncrement());
+        patients.add(patient);
+    }
+
+    public boolean deleteById(int id) {
+        return patients.removeIf(patient -> patient.getId() == id);
+    }
+}
