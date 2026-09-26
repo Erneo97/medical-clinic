d8be07e3c4292e82a382754e6b20f1cd3c24413a
Erneo97 <michalkaniewski1997@gmail.com>
2026-09-26 22:59:51 +0200

    feat: return code 409 when patient with email exist

--- src/main/java/com/github/erneo97/medical_clinic/service/exception/PatientAlreadyExistsException.java
+++ src/main/java/com/github/erneo97/medical_clinic/service/exception/PatientAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.github.erneo97.medical_clinic.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class PatientAlreadyExistsException extends RuntimeException {
     public PatientAlreadyExistsException(String message) {
         super(message);
